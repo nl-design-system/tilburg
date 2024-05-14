b4ae4fcb5500ca2e5492f6e6ad3986d6e44bfaab
markacato <128050096+markacato@users.noreply.github.com>
2024-05-14 15:09:51 +0000

    Deploying to gh-pages from @ nl-design-system/tilburg@87f516649cfebb427c4c76fc599791ac6e9566c6 🚀

--- assets/WithTooltip-Y7J54OF7-CCJiUOhQ.js
+++
@@ -1,1 +0,0 @@
-import{W as e,W as h,b as l}from"./chunk-HLWAVYOI-nWmMCZQY.js";import"./iframe-BuxTPPSL.js";import"../sb-preview/runtime.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./react-18-CzKcEif8.js";import"./extends-CCbyfPlC.js";import"./index-ChwMrSJI.js";export{e as WithToolTipState,h as WithTooltip,l as WithTooltipPure};
---
+++ assets/css-utrecht-unordered-list.stories-CSYFAe84.js
@@ -0,0 +1,15 @@
+import{j as n}from"./jsx-runtime-Nms4Y4qS.js";import{U as s,a}from"./index-iRzvGoSg.js";import"./index-BwDkhjyp.js";import"./_commonjsHelpers-BosuxZz1.js";import"./extends-CCbyfPlC.js";const d=`# Unordered List
+
+This is an unordered list.
+`,u={title:"CSS Component/Unordered List",id:"css-utrecht-unordered-list",component:s,argTypes:{children:{name:"Content",type:{name:"string",required:!0},defaultValue:""}},args:{children:""},tags:["autodocs"],parameters:{design:{type:"figma",url:"https://www.figma.com/design/ck81CE8SNzePi30jCEu7MK/NLDS---Gemeente-Tilburg---Bibliotheek?node-id=828%3A920&t=Y2YZ0H2902enM7la-1"},docs:{description:{component:d}}}},e={name:"Unordered list",args:{children:[n.jsx(a,{children:"In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)"}),n.jsx(a,{children:"In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)"}),n.jsx(a,{children:"In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)"})]}};var r,t,i;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Unordered list',
+  args: {
+    children: [<UnorderedListItem>
+        In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)
+      </UnorderedListItem>, <UnorderedListItem>
+        In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)
+      </UnorderedListItem>, <UnorderedListItem>
+        In deze app staan niet de dagen waarop wij de wijkcontainers legen, alleen de huiscontainers (kliko’s)
+      </UnorderedListItem>]
+  }
+}`,...(i=(t=e.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,u as default};
