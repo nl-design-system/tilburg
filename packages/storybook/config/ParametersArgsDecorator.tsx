70f163ef4348edc33cf5dd89fd5c0ce2e5f8ce34
Joost Wiechers <joostert@gmail.com>
2024-05-15 17:20:24 +0200

    Added table stories

--- packages/storybook/config/ParametersArgsDecorator.tsx
+++ packages/storybook/config/ParametersArgsDecorator.tsx
@@ -1,11 +1,14 @@
 import type { Decorator } from '@storybook/react';
+import { Document } from '@utrecht/component-library-react';
 
 export const ParametersArgsDecorator: Decorator = (Story, context) => {
   context.parameters['args'] = context.args;
 
   return (
     <div className="tilburg-theme">
-      <Story />
+      <Document>
+        <Story />
+      </Document>
     </div>
   );
 };
