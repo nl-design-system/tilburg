b652a1855bfb6316fa69690e2e56136d77a3282e
matijs <matijs@gmail.com>
2024-05-16 09:21:48 +0200

    fix: add missing <Document> wrapper (#16)

--- packages/storybook/config/ParametersArgsDecorator.tsx
+++ packages/storybook/config/ParametersArgsDecorator.tsx
@@ -1,5 +1,5 @@
 import type { Decorator } from '@storybook/react';
-import { Document } from '@utrecht/component-library-react';
+import { Document } from '@utrecht/component-library-react/dist/css-module';
 
 export const ParametersArgsDecorator: Decorator = (Story, context) => {
   context.parameters['args'] = context.args;
