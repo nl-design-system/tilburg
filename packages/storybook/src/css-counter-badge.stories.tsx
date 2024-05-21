24412f2b9d9f8508b607030978ff1063dd6134d5
Mark Teekman <128050096+markacato@users.noreply.github.com>
2024-05-21 14:34:36 +0200

    feat: add counter badge (#23)
    Co-authored-by: Mark Teekman <mark.teekman@gmail.com>

---
+++ packages/storybook/src/css-counter-badge.stories.tsx
@@ -0,0 +1,92 @@
+/* @license CC0-1.0 */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { BadgeCounter } from '@utrecht/component-library-react/dist/css-module';
+import readme from './documentation/counter-badge.md?raw';
+
+const meta = {
+  title: 'CSS Component/Counter badge',
+  id: 'css-utrecht-badge-counter',
+  component: BadgeCounter,
+  argTypes: {
+    children: {
+      name: 'Content',
+      type: {
+        name: 'string',
+        required: true,
+      },
+      defaultValue: '',
+    },
+  },
+  args: {
+    children: '',
+  },
+  tags: ['autodocs'],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/ck81CE8SNzePi30jCEu7MK/NLDS---Gemeente-Tilburg---Bibliotheek?node-id=1235%3A4364&t=VEgVZGuEwb18Kx3g-1',
+    },
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} satisfies Meta<typeof BadgeCounter>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Single: Story = {
+  name: 'Single',
+  args: {
+    children: '9',
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/ck81CE8SNzePi30jCEu7MK/NLDS---Gemeente-Tilburg---Bibliotheek?node-id=1235%3A4364&t=VEgVZGuEwb18Kx3g-1',
+    },
+  },
+};
+
+export const Double: Story = {
+  name: 'Double',
+  args: {
+    children: '99',
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/ck81CE8SNzePi30jCEu7MK/NLDS---Gemeente-Tilburg---Bibliotheek?node-id=1235%3A4364&t=VEgVZGuEwb18Kx3g-1',
+    },
+  },
+};
+
+export const Triple: Story = {
+  name: 'Triple',
+  args: {
+    children: '999',
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/ck81CE8SNzePi30jCEu7MK/NLDS---Gemeente-Tilburg---Bibliotheek?node-id=1235%3A4364&t=VEgVZGuEwb18Kx3g-1',
+    },
+  },
+};
+
+export const Quadruple: Story = {
+  name: 'Quadruple',
+  args: {
+    children: '9.999',
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/ck81CE8SNzePi30jCEu7MK/NLDS---Gemeente-Tilburg---Bibliotheek?node-id=1235%3A4364&t=VEgVZGuEwb18Kx3g-1',
+    },
+  },
+};
