e8918e469d5c5b96f160fba298004b8c4c34de9e
Roel Berends <r.berends@ditpsoftware.nl>
2024-08-14 11:50:09 +0200

    feat: added button-group stories

--- packages/component-library-angular/src/button-group/component.ts
+++ packages/component-library-angular/src/button-group/component.ts
@@ -1,11 +1,9 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'tilburg-button-group',
   templateUrl: 'index.html',
   styleUrls: ['index.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
 })
 export class TilburgButtonGroup {
   constructor() {}
---
+++ packages/component-library-angular/stories/ButtonGroup/Button.stories.ts
@@ -0,0 +1,26 @@
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { TilburgButtonGroup, TilburgComponentsModule } from '@tilburg/component-library-angular';
+
+export default {
+  title: 'Tilburg Angular Components/Button Group',
+  id: 'button-group',
+  component: TilburgButtonGroup,
+  decorators: [
+    moduleMetadata({
+      imports: [TilburgComponentsModule],
+    }),
+  ],
+} as Meta;
+
+type Story = StoryObj<TilburgButtonGroup>;
+
+export const Default: Story = {
+  render: (args) => ({
+    props: args,
+    template:
+      '<tilburg-button-group>' +
+      '<tilburg-button appearance="primary-action-button">Save and continue</tilburg-button>' +
+      '<tilburg-button appearance="secondary-action-button">Back</tilburg-button>' +
+      '</tilburg-button-group>',
+  }),
+};
