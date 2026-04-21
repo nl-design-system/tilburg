9ac1cf5a02ffc8304b7487b1669c3d110bc6f613
Nick Plooij <n.plooy@ditpsoftware.nl>
2026-04-21 16:44:38 +0200

    chore: remove storybook

--- packages/components-angular/.storybook/manager.ts
+++
@@ -1,7 +0,0 @@
-import { addons } from '@storybook/manager-api';
-import { create, ThemeVars } from '@storybook/theming';
-import { theme } from './storybook-theme';
-
-addons.setConfig({
-  theme: create(theme as ThemeVars),
-});
